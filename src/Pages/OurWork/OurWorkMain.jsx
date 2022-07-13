c87db71e7c1d533edbb5b6bdc92a646af3899c55
AvetBadalyan <avetbadalyan@gmail.com>
2022-07-14 01:44:52 +0400

    feat: add left part of Our Work pages main content

--- src/Pages/OurWork/OurWorkMain.jsx
+++ src/Pages/OurWork/OurWorkMain.jsx
@@ -1,50 +1,44 @@
 import React from "react";
 import "./OurWorkMain.css";
-import filter1 from "./../../assets/our works/filters/01.all.png";
-import filter2 from "./../../assets/our works/filters/02.detached.png";
-import filter3 from "./../../assets/our works/filters/03.semi detached.png";
-import filter4 from "./../../assets/our works/filters/04.condo.png";
-import filter5 from "./../../assets/our works/filters/05.estates.png";
-import filter6 from "./../../assets/our works/filters/06.luxury.png";
-import filter7 from "./../../assets/our works/filters/07.modern.png";
-import filter8 from "./../../assets/our works/filters/08.traditional.png";
-import filter9 from "./../../assets/our works/filters/09.classic.png";
+import OurWorkTop from "./OurWorkTop";
+import play from "./../../assets/our works/Vector.png";
 
 export default function OurWorkMain() {
-  const categories = [
-    { filter: filter1, title: "All" },
-    { filter: filter2, title: "Detached" },
-    { filter: filter3, title: "Semi detached" },
-    { filter: filter4, title: "Condo(hover)" },
-    { filter: filter5, title: "Estates" },
-    { filter: filter6, title: "Luxury" },
-    { filter: filter7, title: "Modern" },
-    { filter: filter8, title: "Traditional" },
-    { filter: filter9, title: "Classic" },
-  ];
   return (
     <div className="our-work-main">
-      <div className="our-work-top-part">
-        <div className="empty-div"></div>
-        <h1>OUR WORK</h1>
-        <div className="filter-menu">
-          <div className="filter-menu-text">Filter by</div>
-          {categories.map((category) => {
-            return (
-              <button className="filter-category-button">
-                <img src={category.filter} alt="category-image" />
-                <div>{category.title}</div>
-                <div className="empty-div"></div>
-              </button>
-            );
-          })}
-          <div className="clear-filter-button">
-            <span>X</span>
-            <p>Clear</p>
+      <OurWorkTop />
+      <div className="our-work-center">
+        <div className="our-work-main-left">
+          <div>
+            <span>Starting </span>
+            <span>
+              99<sup>$</sup>
+            </span>
+          </div>
+          <div>
+            <h1>REAL ESTATE CINEMATIC VIDEO</h1>
+          </div>
+          <div className="paragraph-real-estate">
+            <p>
+              Video is THE most effective content type in today's digital
+              marketing environment. We offer a truly CUSTOM Video production
+              for Real Estate, from music selection to editing style, every
+              detail will be created to best showcase your property. On average,
+              2 -4 min, dynamic Cinematic Experience is THE most effective way
+              to create strong engagement and generate leads. Delivery: NEXT
+              DAY.
+            </p>
+          </div>
+          <div className="real-estate-buttons">
+            <button className="order-now-button">Order now</button>
+            <button className="how-it-works-button">
+              <img src={play} alt="play button" />
+        How it works
+            </button>
           </div>
         </div>
+        <div className="our-work-main-right"></div>
       </div>
-      <div className="our-work-center"> Main part</div>
     </div>
   );
 }
---
+++ src/Pages/OurWork/OurWorkTop.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./OurWorkTop.css";
+import filter1 from "./../../assets/our works/filters/01.all.png";
+import filter2 from "./../../assets/our works/filters/02.detached.png";
+import filter3 from "./../../assets/our works/filters/03.semi detached.png";
+import filter4 from "./../../assets/our works/filters/04.condo.png";
+import filter5 from "./../../assets/our works/filters/05.estates.png";
+import filter6 from "./../../assets/our works/filters/06.luxury.png";
+import filter7 from "./../../assets/our works/filters/07.modern.png";
+import filter8 from "./../../assets/our works/filters/08.traditional.png";
+import filter9 from "./../../assets/our works/filters/09.classic.png";
+
+export default function OurWorkTop() {
+  const categories = [
+    { filter: filter1, title: "All" },
+    { filter: filter2, title: "Detached" },
+    { filter: filter3, title: "Semi detached" },
+    { filter: filter4, title: "Condo(hover)" },
+    { filter: filter5, title: "Estates" },
+    { filter: filter6, title: "Luxury" },
+    { filter: filter7, title: "Modern" },
+    { filter: filter8, title: "Traditional" },
+    { filter: filter9, title: "Classic" },
+  ];
+
+  return (
+    <div className="our-work-top-part">
+      <div className="empty-div"></div>
+      <h1>OUR WORK</h1>
+      <div className="filter-menu">
+        <div className="filter-menu-text">Filter by</div>
+        {categories.map((category) => {
+          return (
+            <button className="filter-category-button">
+              <img src={category.filter} alt="category-image" />
+              <div>{category.title}</div>
+              <div className="empty-div"></div>
+            </button>
+          );
+        })}
+        <div className="clear-filter-button">
+          <span>X</span>
+          <p>Clear</p>
+        </div>
+      </div>
+    </div>
+  );
+}
