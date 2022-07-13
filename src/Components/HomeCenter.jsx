87041a20d5f7e52dfac696771c0f2c28c7da5570
AvetBadalyan <avetbadalyaN@gmail.com>
2022-07-13 13:42:29 +0400

    feat: central part of home page

--- src/Components/HomeCenter.jsx
+++ src/Components/HomeCenter.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import "./HomeCenter.css"
 
 export default function HomeCenter() {
-  return (
-    <div>HomeCenter</div>
-  )
+    return (
+      <div className="center">
+        <div className="center-text">CAPTIVATING PROPERY TOURS</div>
+        <div className="center-buttons">
+          <button className='button1'>Pricing (hover)</button>
+          <button className='button2'>Our Work</button>
+          <button className='button3'>Order a Tour</button>
+        </div>
+      </div>
+    );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../Components/Header';
+import HomeCenter from '../../Components/HomeCenter';
 import "./Home.css"
 
 export default function Home() {
   return (
       <div className='home'>
-          <Header/>
+      <Header />
+      <HomeCenter />
     </div>
   )
 }
