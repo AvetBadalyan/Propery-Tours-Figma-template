8a362467acd3b8d6d65f6339aecf6a7eb05ef683
AvetBadalyan <avetbadalyan@gmail.com>
2022-07-15 00:42:25 +0400

    feat: make home page responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,34 +1,39 @@
 import React from "react";
 import "./Header.css";
 import telephone from "./../assets/telephone.png";
-import cart from "./../assets/cart-icon.png"
+import cart from "./../assets/cart-icon.png";
 
 export default function Header() {
   return (
-    <div>
+    <div className="header">
+      <div className="empty-div"></div>
       <div className="header-mid">
         <p>PRICING</p>
         <p>ORDER A TOUR</p>
         <p>SERVICES</p>
       </div>
 
-      <div className="telephone-box">
-        <img src={telephone} alt="telephone" />
-        <p className="telephone">(905) 766 9009</p>
-      </div>
+      <div className="header-right">
+        <div className="telephone-box">
+          <img src={telephone} alt="telephone" />
+          <p className="telephone">(905) 766 9009</p>
+        </div>
 
-      <div className="cart-icon">
-        <img src={cart} alt="cart-icon" />
-      </div>
+        <div className="cart-icon-text">
+          <div className="cart-icon">
+            <img src={cart} alt="cart-icon" />
+          </div>
 
-      <div className="cart-text">
-        <p>Cart (0)</p>
-      </div>
+          <div className="cart-text">
+            <p>Cart (0)</p>
+          </div>
+        </div>
 
-      <div>
-        <button className="signup-button">
-          <p className="button-text">Sign in/Sign up</p>
-        </button>
+        <div>
+          <button className="signup-button">
+            <p className="button-text">Sign in/Sign up</p>
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/Components/HomeCenter.jsx
+++ src/Components/HomeCenter.jsx
@@ -3,12 +3,14 @@ import "./HomeCenter.css"
 
 export default function HomeCenter() {
     return (
-      <div className="center">
-        <div className="center-text">CAPTIVATING PROPERY TOURS</div>
-        <div className="center-buttons">
-          <button className='button1'>Pricing (hover)</button>
-          <button className='button2'>Our Work</button>
-          <button className='button3'>Order a Tour</button>
+      <div className="home-main">
+        <div className='home-main-center-text-buttons'>
+          <div className="center-text">CAPTIVATING PROPERY TOURS</div>
+          <div className="center-buttons">
+            <button className="button1">Pricing (hover)</button>
+            <button className="button2">Our Work</button>
+            <button className="button3">Order a Tour</button>
+          </div>
         </div>
       </div>
     );
