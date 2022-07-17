3414c17ea8150e91451eb4eca31d2d557070bafc
AvetBadalyan <avetbadalyan@gmail.com>
2022-07-17 15:32:14 +0400

    feat: dynamically display the additional services cards with its images and texts

--- src/Pages/AdditionalServices/AdditionalServices.jsx
+++ src/Pages/AdditionalServices/AdditionalServices.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import "./AdditionalServices.css"
-    
+import "./AdditionalServices.css";
+import photo1 from "./../../assets/additional services/15.Still photography.png";
+import photo2 from "./../../assets/additional services/26.AGENT VIDEO PRESENTATION.jpg";
+import photo3 from "./../../assets/additional services/3.scan.png";
+import photo4 from "./../../assets/additional services/4.DRONE TRANSPORT.png";
+import photo5 from "./../../assets/additional services/15.Still photography.png";
+import photo6 from "./../../assets/additional services/26.AGENT VIDEO PRESENTATION.jpg";
+
 export default function AdditionalServices() {
   const additionalServicesCards = [
     {
-      img: "img",
+      img: photo1,
       title: "Twilight Photography",
       price: 129,
       option1: "24 hours delivery guarantee",
@@ -12,15 +18,15 @@ export default function AdditionalServices() {
       option3: "24 hours delivery guarantee",
     },
     {
-      img: "img",
-      title: "Agent Video Presentation(hover)",
+      img: photo2,
+      title: "Agent Video Presentation",
       price: 129,
       option1: "24 hours delivery guarantee",
       option2: "x20 HDR professional photos",
       option3: "24 hours delivery guarantee",
     },
     {
-      img: "img",
+      img: photo3,
       price: 129,
       title: "Matterport",
       option1: "24 hours delivery guarantee",
@@ -28,7 +34,7 @@ export default function AdditionalServices() {
       option3: "24 hours delivery guarantee",
     },
     {
-      img: "img",
+      img: photo4,
       price: 129,
       title: "Drone",
       option1: "24 hours delivery guarantee",
@@ -36,7 +42,7 @@ export default function AdditionalServices() {
       option3: "24 hours delivery guarantee",
     },
     {
-      img: "img",
+      img: photo5,
       price: 129,
       title: "Twilight Photography",
       option1: "24 hours delivery guarantee",
@@ -44,7 +50,7 @@ export default function AdditionalServices() {
       option3: "24 hours delivery guarantee",
     },
     {
-      img: "img",
+      img: photo6,
       price: 129,
       title: "Agent Video Presentation",
       option1: "24 hours delivery guarantee",
@@ -64,7 +70,23 @@ export default function AdditionalServices() {
       </div>
       <div className="additional-services-cards">
         {additionalServicesCards.map((serviceCard) => (
-          <div key={Math.random()} className="single-additional-card">service card</div>
+          <div key={Math.random()} className="single-additional-card">
+            <img src={serviceCard.img} alt="image" />
+            <div className="price">
+              <p>
+                <sup>$</sup>
+              </p>
+              <h1>{serviceCard.price}</h1>
+            </div>
+            <h2>{serviceCard.title}</h2>
+            <button className="additional-services-button">Add to Order</button>
+            <ul>
+              <li>{serviceCard.option1}</li>
+              <li>{serviceCard.option2}</li>
+              <li>{serviceCard.option3}</li>
+            </ul>
+            <button className="quick-preview-button">Quick Preview</button>
+          </div>
         ))}
       </div>
     </div>
